48fd82bda439949003f996ee5a3d312a6397d47e
Huynh Thanh Dat <htdat2407@gmail.com>
2025-09-09 19:42:30 +0700

    update 09/09

--- server.js/index.js
+++ server.js/index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 3000;
 const morgan = require('morgan');
 const hbs = require('express-handlebars');
 const route = require('../src/routes');
